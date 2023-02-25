3827206937afbf4bccc8ec27bdcba71460738c93
creatorQuen <spastime@yandex.ru>
2023-02-25 21:42:01 +0300

    material: react elements

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log('Hello World!s');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//const hw = <h1>Hello World!</h1>
+//const hw = React.createElement('h1', null, 'Hello World!')
+const hw = (
+<div>
+  <h1>My Todo list</h1>
+  <input placeholder='search' />
+  <ul>
+    <li>Learn React</li>
+    <li>Build Application</li>
+  </ul>
+</div>
+);
+
+ReactDOM.render(hw, 
+  document.getElementById('root'));
