76a3e533aaf0c14b25cb99ac415da85cb18075da
creatorQuen <spastime@yandex.ru>
2023-02-26 12:17:37 +0300

    material: react components

--- src/index.js
+++ src/index.js
@@ -3,16 +3,37 @@ import ReactDOM from 'react-dom';
 
 //const hw = <h1>Hello World!</h1>
 //const hw = React.createElement('h1', null, 'Hello World!')
-const hw = (
-<div>
-  <h1>My Todo list</h1>
-  <input placeholder='search' />
-  <ul>
-    <li>Learn React</li>
-    <li>Build Application</li>
-  </ul>
-</div>
-);
 
-ReactDOM.render(hw, 
+const DoThisList = () => {
+  return (
+    <ul>
+      <li>Learn React</li>
+      <li>Build Application</li>
+    </ul>
+  );
+};
+
+const AppHeader = () => {
+  return (
+    <h1>My Do This list</h1>
+  );
+};
+
+const SearchPanel = () => {
+  return (
+    <input placeholder='search' />
+  );
+};
+
+const App = () => {
+  return(
+    <div>
+      <AppHeader />
+      <SearchPanel />
+      <DoThisList />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, 
   document.getElementById('root'));
