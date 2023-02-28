2aca8e077c84ce75eebab8425b71973fc94beb14
creatorQuen <spastime@yandex.ru>
2023-02-28 20:53:05 +0300

    feat: delete item by setState and use new array

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,31 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
 import DoThisList from '../dothis-list';
 import ItemStatusFilter from '../item-status-filter';
 
-const App = () => {
-  const doThisData = [
-    {id: 1, label: 'Know about React', important: false},
-    {id: 2, label: 'Build applitcation', important: true},
-    {id: 3, label: 'Deploy to the server', important: false}
-  ];
+export default class App extends Component{
 
-  return(
-    <div className="dothis-app">
-      <AppHeader doThis={1} done={3}/>
-      <div className="top-panel d-flex">
-        <SearchPanel />
-        <ItemStatusFilter />
+  state = {
+    doThisData: [
+      {id: 1, label: 'Know about React', important: false},
+      {id: 2, label: 'Build applitcation', important: true},
+      {id: 3, label: 'Deploy to the server', important: false}
+    ]
+  };
+
+  deleteItem = (id) => {
+    this.setState(({doThisData})=> {
+      const idx = doThisData.findIndex((el) => el.id === id);
+      // console.log(idx);
+      // doThisData.splice(idx, 1);
+
+      const before = doThisData.slice(0, idx);
+      const after = doThisData.slice(idx + 1);
+      const newArray = [...before, ...after];
+
+      return {
+        doThisData: newArray
+      };
+    });
+  }
+
+  render() {
+    return(
+      <div className="dothis-app">
+        <AppHeader doThis={1} done={3}/>
+        <div className="top-panel d-flex">
+          <SearchPanel />
+          <ItemStatusFilter />
+        </div>
+  
+        <DoThisList 
+          doThises={this.state.doThisData} 
+          //onDeleted={ (id) => console.log('Del', id)}
+          onDeleted={this.deleteItem}
+          />
       </div>
-
-      <DoThisList 
-        doThises={doThisData} 
-        onDeleted={ (id) => console.log('Del', id)}
-        />
-    </div>
-  );
+    );
+  };
 };
-
-export default App;
